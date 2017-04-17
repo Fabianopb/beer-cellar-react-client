f5dabc85779912eaaf932003f35ba3d3199e47e8
Fabiano Brito <fabianopbrito@gmail.com>
2017-04-17 12:05:37 +0300

    Adds dumb views for Home and About.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 import Layout from './layout/layout';
+import Home from './views/home';
+import About from './views/about';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Layout>
-          <Route exact path="/" />
-          <Route path="/about" />
-          <Route path="/topics" />
+          <Route exact path="/" component={ Home } />
+          <Route path="/about" component={ About } />
         </Layout>
       </Router>
     );
--- src/layout/layout.js
+++ src/layout/layout.js
@@ -8,7 +8,6 @@ class Layout extends Component {
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
-          <li><Link to="/topics">Topics</Link></li>
         </ul>
         <hr/>
         {this.props.children}
