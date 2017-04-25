6826cc8a27678e9cf195c01dc1fd593a2577f456
Fabiano Brito <fabianopbrito@gmail.com>
2017-04-25 18:32:50 +0300

    Adds dumb link to edit and to remove a beer.

--- src/components/beer.js
+++ src/components/beer.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 
 class Beer extends Component {
 
+  _editBeer() {
+    console.log('clicked to edit beer', this.props);
+  }
+
+  _removeBeer() {
+    console.log('clicked to remove beer', this.props);
+  }
+
   render() {
     return (
       <div>
         Beer {this.props.name} from {this.props.country}
+        <span> | </span><a href="#" onClick={ this._editBeer.bind(this) }>edit</a>
+        <span> | </span><a href="#" onClick={ this._removeBeer.bind(this) }>X</a>
       </div>
     );
   }
